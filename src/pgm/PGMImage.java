69142fda83e5d5e250b9894a66cb19f846b260b8
Ophir LOJKINE <pere.jobs@gmail.com>
2014-12-02 09:02:58 +0100

    Add a method to scale images

--- src/pgm/PGMImage.java
+++ src/pgm/PGMImage.java
@@ -129,23 +129,49 @@ public class PGMImage {
 	/**
 	 * Returns a black&white image generated from the current image
 	 * 
-	 * @param level the level (between 0 and 255) above which a pixel is considered white
+	 * @param level
+	 *            the level (between 0 and 255) above which a pixel is
+	 *            considered white
 	 * @return
 	 */
 	public PGMImage threshold(int level) {
 		byte[] data = new byte[pixels.length];
 		for (int i = 0; i < pixels.length; i++) {
-			data[i] = ((int)pixels[i] > level) ? (byte)0xFF : (byte)0x00;
+			data[i] = ((int) pixels[i] > level) ? (byte) 0xFF : (byte) 0x00;
 		}
 		return new PGMImage(width, height, data);
 	}
 
 	/**
 	 * Returns a black&white image with level set at 127
+	 * 
 	 * @return
 	 */
 	public PGMImage threshold() {
 		return threshold(0);
+	}
+
+	/**
+	 * Scales the image up or down.
+	 * 
+	 * @param factor
+	 *            the ratio between the width of the returned image and the
+	 *            width of the current image
+	 * @return The scaled image
+	 */
+	public PGMImage scale(float factor) {
+		int w = (int)(factor*width);
+		int h = (int)(factor*height);
+		byte[] data = new byte[pixels.length];
+		// Iteration over the pixels of the new image
+		for (int x = 0; x < w; x++) {
+			for (int y = 0; y < h; y++) {
+				int oldx = Math.min((int)(x/factor), w-1);
+				int oldy = Math.min((int)(y/factor), h-1);
+				data[x+y*w] = pixels[oldx+oldy*width]; 
+			}
+		}
+		return new PGMImage(width, height, data);
 	}
 
 }
